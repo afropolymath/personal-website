5fa8091c04169fbde77a69730d4883a315ae836f
Chidiebere Nnadi <chidiebere.nnadi@gmail.com>
2016-04-17 17:17:24 +0100

    Final modifications on site

--- app/modules/core/js/controllers.js
+++ app/modules/core/js/controllers.js
@@ -1,39 +1,16 @@
 angular.module('core.controllers', [])
 .controller('CoreHomeController', ['$scope', '$timeout', 'CoreHomeService', function($scope, $timeout, CoreHomeService) {
+  $scope.selectedSection = 'build';
   $scope.loading = false;
   $scope.skills = [
-    {
-      name: "JavaScript",
-      value: 70
-    },
-    {
-      name: "AngularJS",
-      value: 80
-    },
-    {
-      name: "NodeJS",
-      value: 60
-    },
-    {
-      name: "PHP",
-      value: 90
-    },
-    {
-      name: "CSS",
-      value: 95
-    },
-    {
-      name: "UI Design",
-      value: 70
-    },
-    {
-      name: "Java",
-      value: 60
-    },
-    {
-      name: "Mobile Application Dev.",
-      value: 70
-    }
+    { icon: "mdi-language-python", name: "Python", value: 70 },
+    { icon: "mdi-language-javascript", name: "JavaScript + NodeJS", value: 80 },
+    { icon: "mdi-language-javascript", name: "AngularJS", value: 80 },
+    { icon: "mdi-language-php", name: "PHP", value: 90 },
+    { icon: "mdi-language-css3", name: "CSS", value: 95 },
+    { icon: "mdi-language-html5", name: "UI Design", value: 70 },
+    { icon: "mdi-code-braces", name: "Java", value: 60 },
+    { icon: "mdi-android", name: "Mobile Application Dev.", value: 70 }
   ];
   $scope.sendMessage = function() {
     $scope.loading = true;
